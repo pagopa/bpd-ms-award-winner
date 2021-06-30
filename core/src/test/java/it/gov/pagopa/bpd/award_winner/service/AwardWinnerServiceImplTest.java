79369618fd5ed0d10b7a9e71036cd3a3e83be89a
mdesogus <michael.desogus.sia@gmail.com>
2021-06-30 21:23:42 +0200

    Fixed test on AwardWinnerServiceImplTest

--- core/src/test/java/it/gov/pagopa/bpd/award_winner/service/AwardWinnerServiceImplTest.java
+++ core/src/test/java/it/gov/pagopa/bpd/award_winner/service/AwardWinnerServiceImplTest.java
@@ -14,11 +14,13 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:config/awardWinnerService.properties")
 @ContextConfiguration(classes = AwardWinnerServiceImpl.class)
 public class AwardWinnerServiceImplTest extends BaseTest {
 
