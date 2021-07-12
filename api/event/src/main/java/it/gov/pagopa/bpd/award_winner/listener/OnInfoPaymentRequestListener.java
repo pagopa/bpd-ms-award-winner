eab4280e03ee04dac9029e9ae012df35199eceb9
Alessio Cialini <alessio.cialini.sia@gmail.com>
2021-07-12 12:31:40 +0200

    [bpd-ms-award-winner/cst_115_131] Updated listener to include event management inside a single class

--- api/event/src/main/java/it/gov/pagopa/bpd/award_winner/listener/OnInfoPaymentRequestListener.java
+++ api/event/src/main/java/it/gov/pagopa/bpd/award_winner/listener/OnInfoPaymentRequestListener.java
@@ -1,24 +1,19 @@
 package it.gov.pagopa.bpd.award_winner.listener;
 
 import eu.sia.meda.eventlistener.BaseConsumerAwareEventListener;
-import it.gov.pagopa.bpd.award_winner.command.InsertIntegratedPaymentCommand;
-import it.gov.pagopa.bpd.award_winner.command.SavePaymentInfoOnErrorCommand;
-import it.gov.pagopa.bpd.award_winner.command.UpdateAwardWinnerCommand;
+import it.gov.pagopa.bpd.award_winner.command.*;
 import it.gov.pagopa.bpd.award_winner.constants.ListenerHeaders;
 import it.gov.pagopa.bpd.award_winner.listener.factory.ModelFactory;
 import it.gov.pagopa.bpd.award_winner.listener.factory.SaveOnErrorCommandModelFactory;
-import it.gov.pagopa.bpd.award_winner.model.AwardWinnerCommandModel;
-import it.gov.pagopa.bpd.award_winner.model.AwardWinnerErrorCommandModel;
-import it.gov.pagopa.bpd.award_winner.model.IntegratedPaymentCommandModel;
+import it.gov.pagopa.bpd.award_winner.listener.factory.SaveOnIntegrationErrorCommandModelFactory;
+import it.gov.pagopa.bpd.award_winner.model.*;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.kafka.common.header.Header;
 import org.apache.kafka.common.header.Headers;
-import org.apache.kafka.common.header.internals.RecordHeader;
-import org.apache.kafka.common.requests.RequestHeader;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
@@ -33,17 +28,26 @@ import java.util.Arrays;
 @Slf4j
 public class OnInfoPaymentRequestListener extends BaseConsumerAwareEventListener {
 
+    private final ModelFactory<Pair<byte[], Headers>, AwardWinnerIntegrationCommandModel> insertAwardWinnerCommandModelFactory;
+    private final SaveOnIntegrationErrorCommandModelFactory saveOnIntegrationErrorCommandModelFactory;
     private final ModelFactory<Pair<byte[], Headers>, AwardWinnerCommandModel> updateAwardWinnerCommandModelFactory;
     private final ModelFactory<Pair<byte[], Headers>, IntegratedPaymentCommandModel> integratedPaymentCommandModelModelFactory;
     private final SaveOnErrorCommandModelFactory saveAwardWinnerErrorCommandModelFactory;
     private final BeanFactory beanFactory;
 
+    @Value("${listener.OnIntegrationPaymentRequestListener.enableCsvIntegration}")
+    private Boolean enabledCsvIntegrationListener;
+
     @Autowired
     public OnInfoPaymentRequestListener(
+            ModelFactory<Pair<byte[], Headers>, AwardWinnerIntegrationCommandModel> insertAwardWinnerCommandModelFactory,
+            SaveOnIntegrationErrorCommandModelFactory saveOnIntegrationErrorCommandModelFactory,
             ModelFactory<Pair<byte[], Headers>, AwardWinnerCommandModel> updateAwardWinnerCommandModelFactory,
             ModelFactory<Pair<byte[], Headers>, IntegratedPaymentCommandModel> integratedPaymentCommandModelModelFactory,
             SaveOnErrorCommandModelFactory saveAwardWinnerErrorCommandModelFactory,
             BeanFactory beanFactory) {
+        this.insertAwardWinnerCommandModelFactory = insertAwardWinnerCommandModelFactory;
+        this.saveOnIntegrationErrorCommandModelFactory = saveOnIntegrationErrorCommandModelFactory;
         this.updateAwardWinnerCommandModelFactory = updateAwardWinnerCommandModelFactory;
         this.saveAwardWinnerErrorCommandModelFactory = saveAwardWinnerErrorCommandModelFactory;
         this.integratedPaymentCommandModelModelFactory = integratedPaymentCommandModelModelFactory;
@@ -86,20 +90,14 @@ public class OnInfoPaymentRequestListener extends BaseConsumerAwareEventListener
                         UpdateAwardWinnerCommand.class, awardWinnerCommandModel);
 
 
-                    if (!command.execute()) {
-                        throw new Exception("Failed to execute UpdateAwardWinnerCommand");
-                    }
-
-                    if (log.isDebugEnabled()) {
-                        log.debug("UpdateAwardWinnerCommand successfully executed for inbound message");
-                    }
-
-
-                else if(headers.lastHeader(ListenerHeaders.INTEGRATION_PAYMENT_HEADER) == null ||
-                        !Arrays.equals(headers.lastHeader(ListenerHeaders.INTEGRATION_PAYMENT_HEADER).value(),
-                                "true".getBytes())) {
-
+                if (!command.execute()) {
+                    throw new Exception("Failed to execute UpdateAwardWinnerCommand");
                 }
+
+                if (log.isDebugEnabled()) {
+                    log.debug("UpdateAwardWinnerCommand successfully executed for inbound message");
+                }
+
 
             } catch (Exception e) {
 
@@ -195,7 +193,83 @@ public class OnInfoPaymentRequestListener extends BaseConsumerAwareEventListener
                 }
 
             }
+
+        }  else if (enabledCsvIntegrationListener && (headers.toArray() != null && headers.toArray().length > 0 &&
+                Arrays.stream(headers.toArray()).anyMatch(
+                        h -> h.key().equals(ListenerHeaders.INTEGRATION_HEADER)))) {
+
+            AwardWinnerIntegrationCommandModel awardWinnerInsertCommandModel = null;
+            AwardWinnerIntegrationErrorCommandModel awardWinnerInsertErrorCommandModel;
+
+            try {
+
+                if (log.isDebugEnabled()) {
+                    log.debug("Processing new request on inbound queue");
+                }
+
+                awardWinnerInsertCommandModel = insertAwardWinnerCommandModelFactory
+                        .createModel(Pair.of(payload, headers));
+                InsertAwardWinnerCommand command = beanFactory.getBean(
+                        InsertAwardWinnerCommand.class, awardWinnerInsertCommandModel);
+
+                if (headers.lastHeader(ListenerHeaders.INTEGRATION_HEADER) != null &&
+                        Arrays.equals(headers.lastHeader(ListenerHeaders.INTEGRATION_HEADER).value(),
+                                "true".getBytes())) {
+
+                    if (!command.execute()) {
+                        throw new Exception("Failed to execute InsertAwardWinnerCommand");
+                    }
+
+                    if (log.isDebugEnabled()) {
+                        log.debug("InsertAwardWinnerCommand successfully executed for inbound message");
+                    }
+
+                }
+
+            } catch (Exception e) {
+
+                String payloadString = "null";
+                String error = "Unexpected error during transaction processing";
+
+                try {
+                    payloadString = new String(payload, StandardCharsets.UTF_8);
+                } catch (Exception e2) {
+                    if (logger.isErrorEnabled()) {
+                        logger.error("Something gone wrong converting the payload into String", e2);
+                    }
+                }
+
+                if (awardWinnerInsertCommandModel != null && awardWinnerInsertCommandModel.getPayload() != null) {
+                    payloadString = new String(payload, StandardCharsets.UTF_8);
+                    error = String.format("Unexpected error during transaction processing: %s, %s",
+                            payloadString, e.getMessage());
+
+                } else if (payload != null) {
+                    error = String.format("Something gone wrong during the evaluation of the payload: %s, %s",
+                            payloadString, e.getMessage());
+                    if (logger.isErrorEnabled()) {
+                        logger.error(error, e);
+                    }
+                }
+
+                awardWinnerInsertErrorCommandModel = saveOnIntegrationErrorCommandModelFactory
+                        .createModel(Pair.of(payload, headers), error, this);
+
+                SavePaymentIntegrationOnErrorCommand errorCommand = beanFactory.getBean(
+                        SavePaymentIntegrationOnErrorCommand.class, awardWinnerInsertErrorCommandModel);
+
+                if (!errorCommand.execute()) {
+                    throw new Exception("Failed to execute SavePaymentIntegrationOnErrorCommand");
+                }
+
+                if (log.isDebugEnabled()) {
+                    log.debug("InsertAwardWinnerCommand successfully executed for inbound message");
+                }
+
+            }
+
         }
+
     }
 
 }
